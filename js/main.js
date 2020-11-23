76a90307b0032b68ab7797f408ce2b3911d3c88e
Eliyas <eliyas54045@gmail.com>
2020-11-23 16:57:55 -0500

    updates

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+
+sr.reveal('.text-left',{delay: 200});
+sr.reveal('.exp',{delay: 200}); 
+sr.reveal('h3',{delay: 200}); 
+sr.reveal('#animate',{delay: 200}); 
+
+sr.reveal('#projects',{delay: 200});
+sr.reveal('#gsul',{delay: 200});
+sr.reveal('#ed',{delay: 200});
+sr.reveal('#major',{delay: 200});
+sr.reveal('.me',{delay: 200});
+
