c3291810ec20d2efefeadd3fbc48393973088b6c
Eliyas <eliyas54045@gmail.com>
2021-05-25 00:09:29 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -8,14 +8,14 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 
-sr.reveal('.text-left',{delay: 200});
-sr.reveal('.exp',{delay: 200}); 
+
+
 
 sr.reveal('#animate',{delay: 200}); 
 
 sr.reveal('#projects',{delay: 200});
 sr.reveal('#gsul',{delay: 200});
-sr.reveal('#ed',{delay: 200});
-sr.reveal('#major',{delay: 200});
-sr.reveal('.me',{delay: 200});
+
+
+
 
