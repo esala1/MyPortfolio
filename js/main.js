15ee5d31cc5b59f03dd84528da4df4273af60d6f
Eliyas <eliyas54045@gmail.com>
2021-06-20 16:16:35 -0400

    updated about page

--- js/main.js
+++ js/main.js
@@ -15,7 +15,9 @@ sr.reveal('#animate',{delay: 200});
 
 sr.reveal('#projects',{delay: 200});
 sr.reveal('#gsul',{delay: 200});
-
-
-
-
+sr.reveal('#entrepreneur_title',{delay: 200});
+sr.reveal('#entrepreneur_card',{delay: 10});
+sr.reveal('#music',{delay: 10});
+sr.reveal('#projects-row1',{delay: 10});
+sr.reveal('#projects-row2',{delay: 10});
+sr.reveal('#about_div',{delay: 10});
