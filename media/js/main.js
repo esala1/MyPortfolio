b966c656abab1ae78c74f1294057b545e18cafe2
Eliyas <eliyas54045@gmail.com>
2021-09-12 18:47:16 -0400

    updated projects

---
+++ media/js/main.js
@@ -0,0 +1,18 @@
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+
+
+
+
+sr.reveal('#animate',{delay: 200}); 
+sr.reveal('#projects',{delay: 200});
+sr.reveal('#projects-row1',{delay: 10});
+sr.reveal('#projects-row2',{delay: 10});
+
