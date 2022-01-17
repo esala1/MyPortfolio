1821730933532194b6229149e3d1e5cccdac70c4
esala1 <eliyas54045@gmail.com>
2022-01-16 22:15:02 -0500

    fixed bug to enable typewriter effect using vanilla js

--- js/main.js
+++ js/main.js
@@ -13,3 +13,29 @@ sr.reveal('#projects-row2',{delay: 100});
 sr.reveal('#aboutme',{delay: 800}); 
 sr.reveal('#gettoknowme',{delay: 450})
 sr.reveal('#music',{delay: 450})
+sr.reveal('#researchproject',{delay: 450})
+
+/* Typewriter effect*/
+const text = ['Process finished with exit code 0'];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type () {
+  if(count === text.length) {
+    count = 0;
+  }
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+  
+  document.querySelector('.typing').textContent = letter;
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 150);
+  
+}());
+
+
