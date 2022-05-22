97cc841388b22087c21424b700fa66c32e2de400
esala1 <eliyas54045@gmail.com>
2022-05-21 20:55:41 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ sr.reveal('#projects',{delay: 200});
 sr.reveal('#projects-row1',{delay: 100});
 sr.reveal('#projects-row2',{delay: 100});
 sr.reveal('#aboutme',{delay: 800}); 
-sr.reveal('#gettoknowme',{delay: 450})
 sr.reveal('#music',{delay: 450})
 sr.reveal('#researchproject',{delay: 450})
 
